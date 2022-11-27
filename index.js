2ef0911158432217b4875c299ac77ed98c38914d
Hemanth Mudra <47498538+hmswrth@users.noreply.github.com>
2022-11-27 20:24:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = express()
 const sendEmail = require('./api/sendEmail')
 const hello = require('./api/hello')
 const cors = require('cors')
+app.use(cors({
+    origin: '*'
+}));
 
 
 app.use(express.json({extended : false}))
 app.use('/api/sendMessage', sendEmail)
 app.use('/',hello)
-app.use(cors({
-    origin: '*'
-}));
 
 
 const port = process.env.PORT || 8000
